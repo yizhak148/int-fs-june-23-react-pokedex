acc33d2a7eadfd1584f602708baa2a99528ea3a2
Omer Dolev <53278705+dolevoper@users.noreply.github.com>
2024-02-08 13:46:56 +0000

    wip

---
+++ src/Pokemons.tsx
@@ -0,0 +1,11 @@
+export function Pokemons() {
+    return (
+        <menu>
+            <li><a>Abra</a></li>
+            <li><a>Pikachu</a></li>
+            <li><a>Charmander</a></li>
+            <li><a>Ditto</a></li>
+            <li><a>Gengar</a></li>
+        </menu>
+    );
+}
