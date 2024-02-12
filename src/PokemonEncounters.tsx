a1111147e4a27fda3f52fabd4941c7fd19b6241e
Omer Dolev <omerdolev90@gmail.com>
2024-02-12 17:29:32 +0200

    pokemon encounters

--- src/PokemonEncounters.tsx
+++ src/PokemonEncounters.tsx
@@ -1,19 +1,40 @@
+import styles from "./PokemonEncounters.module.scss";
+
 type PokemonEncountersProps = { pokemonName: string };
 
 export function PokemonEncounters({ pokemonName }: PokemonEncountersProps) {
-    return (
-        <article>
-            <h2>{pokemonName} - Encounters</h2>
-            <ul>
-                <li>
-                    <article>
-                        <h3>Red</h3>
-                        <ul>
-                            <li></li>
-                        </ul>
-                    </article>
+  return (
+    <div className={styles.wrapper}>
+      <article className={styles.dialog}>
+        <h2>{pokemonName} - Encounters</h2>
+        <ul className={styles.gameList}>
+          <li>
+            <article>
+              <h3>Red</h3>
+              <ul>
+                <li className={styles.areaListItem}>
+                  kanto-route-13-area (5%)
                 </li>
-            </ul>
-        </article>
-    );
+              </ul>
+            </article>
+          </li>
+          <li>
+            <article>
+              <h3>Blue</h3>
+              <ul>
+                <li className={styles.areaListItem}>
+                  kanto-route-13-area (5%)
+                </li>
+              </ul>
+            </article>
+          </li>
+        </ul>
+        <menu>
+          <li>
+            <a href="">Close</a>
+          </li>
+        </menu>
+      </article>
+    </div>
+  );
 }
