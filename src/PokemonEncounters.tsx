1820e705a7859457b70b3877615a4074221933c9
yizhak148 <yizhak208@gmail.com>
2024-02-15 02:58:45 +0200

    show the palce pokemn show

--- src/PokemonEncounters.tsx
+++ src/PokemonEncounters.tsx
@@ -1,4 +1,38 @@
+import axios from "axios";
 import styles from "./PokemonEncounters.module.scss";
+import { useAsync } from "./useAsync";
+import { useCallback } from "react";
+type pokemonEncounter = {
+  location_area: {
+    name:string
+    url: string
+  },
+  version_details: {
+    max_chance: number,
+    version: {
+      name:string,
+      url: string
+    },
+    encounter_details: {
+      chance: number,
+      max_level: number,
+      min_levl: number,
+      condition_values: {
+        name:string
+        url: string
+      }[]
+    }[]
+  }[]
+
+}
+async function getEncounters( name:string ) {
+  const res = await axios.get< pokemonEncounter[] >(
+    `https://pokeapi.co/api/v2/pokemon/${name}/encounters`
+  );
+
+  return res.data
+}
+
 
 type PokemonEncountersProps = { pokemonName: string; onCloseClick: () => void };
 
@@ -6,6 +40,14 @@ export function PokemonEncounters({
   pokemonName,
   onCloseClick,
 }: PokemonEncountersProps) {
+  const getCurrentPokemonEncounters = useCallback(() => getEncounters("abra"), ["abra"]);
+  const { isLoading, data } = useAsync(getCurrentPokemonEncounters);
+  console.log(data)
+  if (isLoading) {
+    return(
+      <p>loading...</p>
+    )
+  }
   return (
     <div className={styles.wrapper}>
       <article className={styles.dialog}>
@@ -13,10 +55,10 @@ export function PokemonEncounters({
         <ul className={styles.gameList}>
           <li>
             <article>
-              <h3>Red</h3>
+              <h3>{data && data[0].version_details[0].version.name}</h3>
               <ul>
                 <li className={styles.areaListItem}>
-                  kanto-route-13-area (5%)
+                  {data && data[0].location_area.name} ({data && data[0].version_details[0].max_chance}%)
                 </li>
               </ul>
             </article>
